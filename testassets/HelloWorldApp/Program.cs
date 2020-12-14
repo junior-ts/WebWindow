8647d103c5bd9d256f00cfedea791f54b6c58ee5
Progmasys <54924989+Progmasys@users.noreply.github.com>
2020-12-14 20:50:10 -0300

    Update WebView2 SDK to version="1.0.664.37" (Latest Stable). Added x86 support on Windows. Updated README.

--- testassets/HelloWorldApp/Program.cs
+++ testassets/HelloWorldApp/Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorldApp
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             var window = new WebWindow("My great app", options =>
